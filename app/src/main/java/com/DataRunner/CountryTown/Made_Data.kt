ac626fe7f7d76bd72f5511f52dc80d092d0dd82c
qwlake <qwlake@gmail.com>
2020-06-28 19:30:07 +0900

    feat: maker 페이지 스타일 수정 및 이메일 기능 추가

--- app/src/main/java/com/DataRunner/CountryTown/Made_Data.kt
+++
@@ -1,9 +0,0 @@
-package com.DataRunner.CountryTown
-
-class Made_Data(
-    val made_name: String,
-    val made_email: String,
-    val made_git: String,
-    val made_aff: String,
-    val made_pic: String
-)
