cc1a3e8b648e6f6ce71455d64d233cb18949489f
존령 <sapa1313@naver.com>
2020-06-28 16:12:55 +0900

    feat: madeby 정보 추가

---
+++ app/src/main/java/com/DataRunner/CountryTown/Made_Data.kt
@@ -0,0 +1,9 @@
+package com.DataRunner.CountryTown
+
+class Made_Data(
+    val made_name: String,
+    val made_email: String,
+    val made_git: String,
+    val made_aff: String,
+    val made_pic: String
+)
--- app/src/main/java/com/DataRunner/CountryTown/MadebyActivity.kt
+++ app/src/main/java/com/DataRunner/CountryTown/MadebyActivity.kt
@@ -1,17 +1,54 @@
 package com.DataRunner.CountryTown
 
+
 import android.content.Intent
 import android.os.Bundle
+import android.os.StrictMode
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.main_toolbar.*
+import kotlinx.android.synthetic.main.madeby_layout.*
+import org.json.JSONArray
 
 
 class MadebyActivity : AppCompatActivity() {
+    var madeList = arrayListOf<Made_Data>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.madeby_layout)
+
+        parsing()
+    }
+
+    private fun parsing() {
+        val madeByAdapter =  MadeAdapter(this, madeList)
+        madeBy.adapter = madeByAdapter
+        //start
+        StrictMode.enableDefaults()
+        try {
+            val assetManager = resources.assets
+            val inputStream = assetManager.open("made.json")
+            val jsonString = inputStream.bufferedReader().use { it.readText() }
+            val jArray = JSONArray(jsonString)
+
+            // 모든 공지 noticeList 에 저장
+            for (i in 0 until jArray.length()) {
+
+                val obj = jArray.getJSONObject(i)
+
+                val listLine = Made_Data(
+                    obj.getString("이름"),
+                    obj.getString("이메일"),
+                    obj.getString("깃주소"),
+                    obj.getString("소속"),
+                    obj.getString("사진")
+                )
+                madeList.add(listLine)
+            }
+        } catch (e: Exception) {
+            val listLine = Made_Data(e.toString(), "오류", "오류", "오류", "오류")
+            madeList.add(listLine)
+        }
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
