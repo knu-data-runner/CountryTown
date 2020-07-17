d1093de4351e9502eb8816702772285c7f56828f
존령 <sapa1313@naver.com>
2020-07-17 21:56:13 +0900

    feat: '검색'탭 기능 완료

--- app/src/main/java/com/DataRunner/CountryTown/MainActivity.kt
+++ app/src/main/java/com/DataRunner/CountryTown/MainActivity.kt
@@ -18,11 +18,14 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(setOf(
-            R.id.navigation_home,
-            R.id.navigation_search,
-            R.id.navigation_arround,
-            R.id.navigation_more))
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home,
+                R.id.navigation_search,
+                R.id.navigation_arround,
+                R.id.navigation_more
+            )
+        )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
